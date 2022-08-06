6e186072aa0d37756c3a44bd66bbb46040708b75
blue-bigtech <blue.bigtech@gmail.com>
2022-08-06 08:28:50 -0700

    done

---
+++ StudentArray/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using StudentArray.SRC;
+public class Example
+{
+    public static void Main()
+    {
+        bool bExit = false; // 
+        StudentArrayList allStudent = new StudentArrayList();   // this is storage variable that keeps all student data
+        while (!bExit)
+        {
+            Console.WriteLine("Select Function : [1 : Add Student], [2 : Search(ID, Major)] , [3 : Show All Student], [4 : Exit]");
+            int nCmd = Convert.ToInt32(Console.ReadLine());
+            switch (nCmd)
+            {
+                case 1: //When you input 1, you can input new Student data
+                    {
+                        Console.Write("Enter your studentID: ");
+                        int sudentID = Convert.ToInt32(Console.ReadLine()); // input student ID
+
+                        Console.Write("Enter your firstName: ");
+                        string firstName = Console.ReadLine();  // input firstName
+
+                        Console.Write("Enter your lastName: ");
+                        string lastName = Console.ReadLine();   // input lastname
+
+                        Console.Write("Enter your major: ");
+                        string major = Console.ReadLine();  // input major
+
+                        Console.Write("Enter your gpa: ");
+                        double gpa = Convert.ToDouble(Console.ReadLine());  //input gpa
+
+                        Console.Write("Enter your dob: ");
+                        string dob = Console.ReadLine();    // input dob
+
+                        allStudent.add(sudentID, firstName, lastName, major, gpa, dob); // add is StudentArrayList class' member function
+                    }
+                    break;
+                case 2:
+                    Console.Write("no function yet");
+                    break;
+                case 3:
+                    allStudent.showAll(); // showAll is StudentArrayList class' member function
+                    break;
+                case 4:
+                    bExit = true; // app would be closed
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+    }
+}
---
+++ StudentArray/SRC/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace StudentArray.SRC
+{
+    public class StudentArrayList
+    {
+        public ArrayList mStudentList = new ArrayList();    // this is storage vairable in StudentArray
+        public StudentArrayList() // this is constructor when 
+        {
+            this.mStudentList.Clear();  // all data will be deleted
+        }
+        public void add(int studentID=-1, string firstName="", string lastName="", string major="", double gpa=0.0f, string dob="") // every variable has initial value
+        {
+            var oneStudent = new Student(studentID, firstName, lastName, major, gpa, dob);
+            mStudentList.Add(oneStudent);
+        }
+
+        public void showAll() // this method shows all students' data
+        {
+            for (int i=0; i<this.mStudentList.Count; i++)
+            {
+                ((Student)this.mStudentList[i]).showMe();
+            }
+
+        }
+    }
+    public class Student
+    {
+        public int studentID { get; }
+        public string firstName { get; }
+        public string lastName { get; }
+        public string major { get; }
+        public double gpa { get; }
+        public string dob { get; }
+
+        public Student()    // this is constructor
+        {
+            this.studentID = 0;
+            this.firstName = "";
+            this.lastName = "";
+            this.major = "";
+            this.gpa = 0.0f;
+            this.dob = "";
+        }
+
+        public Student(int studentID, string firstName, string lastName, string major, double gpa, string dob)   // this is also constructor with 
+        {
+            this.studentID = studentID;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.major = major;
+            this.gpa = gpa;
+            this.dob = dob;
+        }
+
+        public void showMe() // this method shows one student' data
+        {
+            Console.WriteLine("Student ID : " + this.studentID);
+            Console.WriteLine("  FirstName : " + this.firstName);
+            Console.WriteLine("  LastName : " + this.lastName);
+            Console.WriteLine("  Major : " + this.major);
+            Console.WriteLine("  GPA : " + this.gpa);
+            Console.WriteLine("  Date of Birth : " + this.dob);
+        }
+    }
+}
